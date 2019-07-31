4e7a0d2a40bbe3f2377e56b455b6c213e3532007
hayeon <hayeon56>
2019-07-31 17:39:53 +0900

    sully

--- momoViews/momo.php
+++ momoViews/momo.php
@@ -13,10 +13,10 @@
   <body>
     <h3>ショップサーブ</h3>
       <div>
-        <a href="/momoViews/itemRegistration.php"><p>商品登録</p></a>
+        <a href="../momoViews/itemRegistration.php"><p>商品登録</p></a>
       </div>
       <div>
-        <a href="/momoViews/itemInformation.php"><p>商品情報</p></a>
+        <a href="../momoViews/itemInformation.php"><p>商品情報</p></a>
       </div>
   </body>
 </html>
