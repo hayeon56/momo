087cb915c8986b8a8394ff26583b6020b75ff769
hayeon <hayeon56>
2019-07-31 13:33:31 +0900

    sully

--- momoViews/itemRegistration.php
+++ momoViews/itemRegistration.php
@@ -1,9 +1,9 @@
 <?php
-  include("../momoControllers/momoCategoryController.php");
-
+  include("../momo/momoControllers/momoCategoryController.php");
+  $category = $Momo->category();
   $image = $Momo->image();
 ?>
-<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+ <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
 <script type="text/javascript">
 
     function test(){
