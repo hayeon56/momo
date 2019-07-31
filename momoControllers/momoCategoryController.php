0d9e26163d57645ec637df77133828f4aa73bc2f
hayeon <hayeon56>
2019-07-31 16:16:04 +0900

    sully

--- momoControllers/momoCategoryController.php
+++ momoControllers/momoCategoryController.php
@@ -32,7 +32,7 @@ class Momo{
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_POST, 1);
-  //  curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: json'));
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_USERPWD, $this->shop_id.":".$this->manager_key);
